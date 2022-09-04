1b2cd40d4bffe94f0815beb584445411a3000ed8
Maciej Wawryszuk <maciekwawryszuk@gmail.com>
2022-09-04 18:54:03 +0200

    First Angular App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {keyframes} from "@angular/animations";
+import {config} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'first-project';
+
+// config: { [key: string]: string | Date  }= null ; // QuestionMark - zamiast zwykłej interpolacji wszystkie dane przechowujemy w funkcji config
+
+  // constructor() {
+  //   setTimeout(()=>{//SetTimeout-Asynchorniecznie ustawiamy aby coś wywoływało się z opóźnieniem
+  //     this.config={
+  //       title: 'Lista Zadań',
+  //       footer: 'Lista zadań zbudowana w Angularze',
+  //       date: new Date()
+  //     }
+  //   }, 500)
+  //
+  // }
+title = 'Lista Zadań';
+ //getFooter(): string{ //Interpolacja - wywoływanie tekstu w komponencie html
+ //  return '© Lista zadań,All rights reserved.'
+// }
+  // words = ['jeden', 'dwa','trzy']
+  // num = 9.21234325324;
+  // translate = {
+  //   zima: 'winter',
+  //   wiosna: 'spring',
+  //   lato: 'summer',
+  //   jesień: 'autumn',
+  // };
+  // price = 2.99;
+ getDate(): Date{//Obiekt który wyświetla datę dynamicznie
+     return new Date();
+  }
+footer = '2020 © Lista zadań,All rights reserved.'; //Interpolacja 2 - też działa , wywoływanie tekstu w komponencie html
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
-import { NgModule } from '@angular/core';
+import {LOCALE_ID, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+// \/-Import Lokalizacji dla naszej strony(Domyślnie Angular ma EN_US{Przydaje się jeśli chodzi o daty itd.})
+import localPl from '@angular/common/locales/pl'
+import localBn from '@angular/common/locales/bn'
+import {registerLocaleData} from "@angular/common";
+registerLocaleData(localPl);
+registerLocaleData(localBn);
 @NgModule({
   declarations: [
     AppComponent
@@ -10,7 +15,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [{provide: LOCALE_ID, useValue: 'pl-PL'}],//LocaleID - Nadanie czytania wartości dla angulara aby zmienił lokalizację z EN-US na PL
   bootstrap: [AppComponent]
 })
 export class AppModule { }
